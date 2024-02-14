0783f6af8b721cd3de1f70bcb03a121876fa602a
Héber Herrera <ing.heberh@gmail.com>
2024-02-14 14:36:44 -0600

    Fix assets issues in components

--- src/components/TodoEmpty.jsx
+++ src/components/TodoEmpty.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 export const TodoEmpty = () => {
     return (
         <div className='todo-empty__container d-flex flex-column align-items-center animate__animated animate__fadeIn'>
-            <img src='/assets/clipboard.svg' alt="Clipboard image" width='56px' height='auto' />
+            <img src='./assets/clipboard.svg' alt="Clipboard image" width='56px' height='auto' />
             <h3 className='text-center color-gray-300'>
                 Congratulations! <br />
                 <span className='fw-regular'> You have finished your pendings</span>
