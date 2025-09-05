412a12680dff3c787817ffa4c634998351b53387
Héber Rodrigo Herrera Cruz <ing.heberh@gmail.com>
2025-09-05 13:42:34 -0600

    Feature: 1 Add status Dropdown and refactor main screen

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,6 +1,10 @@
-import { TodoForm, TodoEmpty, TodoList } from "./components/index";
-import { useTodos } from "./hooks/useTodos";
+import {TodoForm, TodoEmpty, TodoList, Dropdown} from "./components/index";
+import {useTodos} from "./hooks/useTodos";
 import 'animate.css'
+import {useState, useMemo} from "react";
+import {showTypes} from "./types";
+import {DropdownItems} from "./constants";
+import {getFilteredTodosByStatus} from "./utils/getTodos.js";
 
 export const TodoApp = () => {
     const {
@@ -18,32 +22,40 @@ export const TodoApp = () => {
     const completed = `${completedTodos} of ${allTodos}`;
     const filteredTodos = useMemo(() => getFilteredTodosByStatus(filterTodosByStatus, todos), [filterTodosByStatus, todos]);
 
-  return (
-    <div className="animate__animated animate__fadeIn">
-      <div className="main__header">
-        <div className="container">
-          <h1 className='text-center'><span className='color-blue'>To</span><span className='color-purple'>do</span></h1>
-          <TodoForm onNewTodo={onAddTodo} />
-        </div>
-      </div>
-
-      <div className="main__body">
-        <div className="container">
-
-          <div className="d-flex justify-content-between">
-
-            <div className="d-flex count__container">
-              <h4 className="color-blue">Todos</h4>
-              <p className="count small">{allTodos}</p>
+    return (
+        <div className="animate__animated animate__fadeIn">
+            <div className="main__header">
+                <div className="container">
+                    <h1 className='text-center'><span className='color-blue'>To</span><span
+                        className='color-purple'>do</span></h1>
+                    <TodoForm onNewTodo={onAddTodo}/>
+                </div>
             </div>
 
-            <div className="d-flex count__container">
-              <h4 className="color-purple">Completed</h4>
-              <p className="count small">
-                {completedTodos}
-              </p>
-            </div>
-          </div>
+            <div className="main__body">
+                <div className="container">
+
+                    <div className="d-flex justify-content-between">
+
+                        <div className="d-flex flex-column d-md-row count__container">
+                            <div className="d-flex count__container">
+                                <h4 className="color-blue">Todos</h4>
+                                <p className="count small">{allTodos}</p>
+                            </div>
+
+                            <Dropdown btnClasses={"color-purple"} btnText={'Show'} dropdownId={'status-dropdown'} dropdownType={'Show items by status'} handleShowTodosBy={setFilterTodosByStatus}
+                                      items={DropdownItems} />
+                        </div>
+
+                        <div className="d-flex flex-column d-md-row count__container">
+                            <div className="d-flex count__container">
+                                <h4 className="color-purple">Completed</h4>
+                                <p className="count small">
+                                    {completed}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
 
                     {(emptyTodos || !filteredTodos.length)
                         ? <TodoEmpty pendingTodos={pendingTodos}/>
